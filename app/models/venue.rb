3a94b4d708431391103eab9838aa612a55d3affc
shvv733kn@gmail.com <shvv733kn@gmail.com>
2017-10-30 20:51:31 +0300

    second

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -23,7 +23,7 @@ class Venue < ApplicationRecord
             res[:office_hours] = office_hours
             res[:dates] = dates
             res[:emails] = emails
-            return {"llel": "dssdds"}
+            return res
         else
             return account.get_attrs
         end
