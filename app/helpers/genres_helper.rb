c73468aebda832f0f9b94689b897983d7844959b
aalbina <aalmukhametova@gmail.com>
2017-12-26 20:17:53 +0300

    genres

--- app/helpers/genres_helper.rb
+++ app/helpers/genres_helper.rb
@@ -1,6 +1,8 @@
 class GenresHelper
 
   def self.all
-    return [:rock, :pop, :rap, :jazz]
+    return [:jazz, :alternative, :blues, :classical, :country, :dance,
+            :electro, :hip_hop, :rap, :indie_pop, :gospel, :k_pop, :jazz,
+            :latin_music, :new_age, :opera, :pop, :soul, :reggae, :rock, :folk, :beats]
   end
 end
