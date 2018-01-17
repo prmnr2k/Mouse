ddeca362f46e0d4590723b56134fa1e538a5726b
aalbina <aalmukhametova@gmail.com>
2018-01-17 23:14:30 +0300

    fan account in comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
   belongs_to :event
   belongs_to :fan
+
+  def as_json(options={})
+    res = super
+    res.delete('fan_id')
+
+    res[:fan] = fan.as_json(only: [:image_id, :user_name])
+
+    return res
+  end
 end
