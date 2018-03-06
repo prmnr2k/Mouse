637f20f0987982e5cc8f238900f63251d4280b2d
aalbina <aalmukhametova@gmail.com>
2018-03-07 00:40:28 +0300

    statuses

--- app/helpers/status_helper.rb
+++ app/helpers/status_helper.rb
@@ -1,5 +1,5 @@
 class StatusHelper
   def self.all
-    return [:pending, :active, :declined, :accepted]
+    return [:pending, :invite_send, :request_send, :accepted, :declined, :owner_accepted, :owner_declined, :time_expired]
   end
 end
