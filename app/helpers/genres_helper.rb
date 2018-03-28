113a70974fc54f9843ee109dbf25a1e887c1ad6f
aalbina <aalmukhametova@gmail.com>
2018-03-28 07:31:42 +0300

    new genres

--- app/helpers/genres_helper.rb
+++ app/helpers/genres_helper.rb
@@ -1,8 +1,9 @@
 class GenresHelper
 
   def self.all
-    return [:alternative, :blues, :classical, :country, :dance, :electro,
-            :hip_hop, :rap, :indie_pop, :gospel, :k_pop, :jazz, :latin_music,
-            :new_age, :opera, :pop, :soul, :reggae, :rock, :folk, :beats]
+    return [:blues, :children_music, :classical, :country, :electronic, :holiday, :opera, :singer, :jazz,
+            :latino, :new_age, :pop, :soul, :musicals, :dance, :hip_hop, :world, :alternative, :rock,
+            :christian_gospel, :vocal, :reggae, :easy_listening, :j_pop, :enka, :anime, :kayokyoku, :k_pop,
+            :karaoke, :instrumental, :brazilian, :spoken_word, :disney, :french_pop, :german_pop, :german_folkc]
   end
 end
