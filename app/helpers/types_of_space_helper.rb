2420695aa639a36750f95f6b233bc499f99c8033
shvv733kn@gmail.com <shvv733kn@gmail.com>
2018-05-07 14:13:12 +0300

    venue_type_add

--- app/helpers/types_of_space_helper.rb
+++ app/helpers/types_of_space_helper.rb
@@ -1,6 +1,6 @@
 class TypesOfSpaceHelper
   def self.all
     return [:night_club, :concert_hall, :event_space, :theatre, :additional_room,
-            :stadium_arena, :outdoor_space, :other, :restaurant, :bar]
+            :stadium_arena, :outdoor_space, :other, :restaurant, :bar, :private_residence]
   end
 end
