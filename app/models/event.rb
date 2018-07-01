4066d078f101e053ee9d0f6859edaa8834f3c6ae
aalbina <aalmukhametova@gmail.com>
2018-07-01 21:59:20 +0300

    tickets count in event

--- app/models/event.rb
+++ app/models/event.rb
@@ -69,6 +69,8 @@ class Event < ApplicationRecord
       res[:venue] = venue.as_json(extended: true)
       res[:venues] = venue_events.order(updated_at: :DESC)
       res[:tickets] = tickets
+      res[:in_person_tickets] = tickets.joins(:tickets_type).where(tickets_types: {name: 'in_person'}).sum('tickets.count')
+      res[:vr_tickets] = tickets.joins(:tickets_type).where(tickets_types: {name: 'vr'}).sum('tickets.count')
     elsif options[:analytics]
       # res[:location] = venue.address if venue
       res[:comments] = comments.count
