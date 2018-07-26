379de45cb7ae6d180685c9a5346285ac68b5a8ac
shvv733kn@gmail.com <shvv733kn@gmail.com>
2018-07-26 23:11:46 +0300

    cal_fix

--- app/models/venue_date.rb
+++ app/models/venue_date.rb
@@ -6,7 +6,7 @@ class VenueDate < ApplicationRecord
 
   def as_json(options = {})
     res = super
-    res.delete('venue_id')
+    #res.delete('venue_id')
     res.delete('created_at')
     res.delete('updated_at')
     return res
