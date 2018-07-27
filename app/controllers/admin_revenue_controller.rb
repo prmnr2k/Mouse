0c1a7b66b9a4d16c6e64bc46a1d1455b6aff7bb6
aalbina <aalmukhametova@gmail.com>
2018-07-28 00:02:41 +0300

    fix revenue

--- app/controllers/admin_revenue_controller.rb
+++ app/controllers/admin_revenue_controller.rb
@@ -57,19 +57,20 @@ class AdminRevenueController < ApplicationController
   swagger_api :counts do
     summary "Get revenue counts"
     param_list :query, :type, :string, :required, "Type of revenue", [:total, :sales]
+    param_list :query, :by, :string, :optional, "Data by", [:day, :week, :month, :year, :all]
     param :header, 'Authorization', :string, :required, 'Authentication token'
     response :not_found
     response :ok
   end
   def counts
-    date = calculate_date(params[:type], nil)
-    artist = calculate_artist(params[:type], nil)
-    venue = calculate_venue(params[:type], nil)
-    vr = calculate_vr(params[:type], nil)
-    tickets = calculate_tickets(params[:type], nil)
-    advertising = calculate_advertising(params[:type], nil)
-    crowdfunding = calculate_crowdfunding(params[:type], nil)
-    regular = calculate_regular(params[:type], nil)
+    date = calculate_date(params[:type], params[:by])
+    artist = calculate_artist(params[:type], params[:by])
+    venue = calculate_venue(params[:type], params[:by])
+    vr = calculate_vr(params[:type], params[:by])
+    tickets = calculate_tickets(params[:type], params[:by])
+    advertising = calculate_advertising(params[:type], params[:by])
+    crowdfunding = calculate_crowdfunding(params[:type], params[:by])
+    regular = calculate_regular(params[:type], params[:by])
 
     render json: {
       date: date,
